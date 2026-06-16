9efc6422f7e733d32c3a44a5564d4b546f31f425
EugenSap <77262638+EugenSap@users.noreply.github.com>
2026-06-16 20:15:47 +0200

    [Технарь] Расширить интерфейс импорта под XLSX

--- src/Pismolet.Web/Application/Imports/IRecipientImportService.cs
+++ src/Pismolet.Web/Application/Imports/IRecipientImportService.cs
@@ -2,5 +2,7 @@ namespace Pismolet.Web.Application.Imports;
 
 public interface IRecipientImportService
 {
+    Task<ImportRecipientsResult> ImportAsync(ImportRecipientsCommand command, CancellationToken cancellationToken = default);
+
     Task<ImportRecipientsResult> ImportCsvAsync(ImportRecipientsCommand command, CancellationToken cancellationToken = default);
 }
