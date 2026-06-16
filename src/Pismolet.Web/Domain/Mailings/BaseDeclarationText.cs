b47e63bcf36469e09224c5183539651fbd981791
EugenSap <77262638+EugenSap@users.noreply.github.com>
2026-06-16 21:20:53 +0200

    [Технарь] Cleanup obsolete Web layer file

--- src/Pismolet.Web/Domain/Mailings/BaseDeclarationText.cs
+++
@@ -1,8 +0,0 @@
-namespace Pismolet.Web.Domain.Mailings;
-
-public static class BaseDeclarationText
-{
-    public const string CurrentVersion = "2026-06-16-v1";
-
-    public const string Text = "Подтверждаю правомерность использования загруженных адресов для этой рассылки, наличие необходимого основания для обращения к адресатам и достоверность выбранного источника базы.";
-}
