93f8d3bfd2043259723e5674f339f1ff40c36720
EugenSap <77262638+EugenSap@users.noreply.github.com>
2026-06-16 21:27:04 +0200

    [Технарь] cleanup

--- src/Pismolet.Web/Application/Imports/IRecipientImportService.cs
+++
@@ -1,8 +0,0 @@
-namespace Pismolet.Web.Application.Imports;
-
-public interface IRecipientImportService
-{
-    Task<ImportRecipientsResult> ImportAsync(ImportRecipientsCommand command, CancellationToken cancellationToken = default);
-
-    Task<ImportRecipientsResult> ImportCsvAsync(ImportRecipientsCommand command, CancellationToken cancellationToken = default);
-}
