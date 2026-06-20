f84f6591b173860bdfe89e907bc23f5e2a7dd36c
EugenSap <77262638+EugenSap@users.noreply.github.com>
2026-06-20 22:50:13 +0200

    [Технарь] Добавить методы fake mailer для ответов

--- src/Pismolet.Application/Mail/IFakeMailer.cs
+++ src/Pismolet.Application/Mail/IFakeMailer.cs
@@ -6,7 +6,16 @@ public interface IFakeMailer
 {
     void SendConfirmation(string to, string subject, string link);
 
-    void AddMailingMessage(string to, string subject, string link);
+    void AddMailingMessage(
+        string to,
+        string subject,
+        string link,
+        string? replyToAddress = null,
+        string? replyToken = null,
+        string? providerMessageId = null,
+        string? textBody = null);
+
+    void AddForwardedReply(string to, string subject, string fromEmail, string textBody, string providerMessageId);
 
     IReadOnlyCollection<FakeMail> GetOutbox();
 }
