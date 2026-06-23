a10af9ea33cfd8968d431cdd78910a56b9c751ce
den1s1 <den1s@mail.ru>
2026-06-23 21:07:04 +0300

    [Технарь] Подключить drill-down доставки по рассылке

--- src/Pismolet.Web/Program.cs
+++ src/Pismolet.Web/Program.cs
@@ -75,6 +75,7 @@ app.UseAuthorization();
 app.UseAdminCampaignDeliveryAnalytics();
 app.UseAdminCampaignOpenAnalytics();
 app.UseAdminCampaignClickAnalytics();
+app.UseAdminDeliveryMailingDrilldown();
 app.UseAdminDeliveryClientDrilldown();
 
 app.MapHomeEndpoints();
