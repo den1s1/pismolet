84ae52c5b78aaef0ef3f873bf03c1cc92ac01540
den1s1 <den1s@mail.ru>
2026-06-25 10:11:51 +0300

    [Технарь] Обновить заготовку шага адресов

--- src/Pismolet.Web/Endpoints/SimplifiedRecipientStepEndpoints.cs
+++ src/Pismolet.Web/Endpoints/SimplifiedRecipientStepEndpoints.cs
@@ -4,6 +4,7 @@ public static class SimplifiedRecipientStepEndpoints
 {
     public static IEndpointRouteBuilder MapSimplifiedRecipientStepEndpoints(this IEndpointRouteBuilder app)
     {
+        // simplified recipients step placeholder
         return app;
     }
 }
