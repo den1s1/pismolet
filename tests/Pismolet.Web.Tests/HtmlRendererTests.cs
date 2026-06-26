2e0810b415101494f67a5692475cbae83f105188
den1s1 <den1s@mail.ru>
2026-06-26 12:24:47 +0300

    [Технарь] Проверять структуру чекбоксов регистрации

--- tests/Pismolet.Web.Tests/HtmlRendererTests.cs
+++ tests/Pismolet.Web.Tests/HtmlRendererTests.cs
@@ -5,16 +5,28 @@ namespace Pismolet.Web.Tests;
 public sealed class HtmlRendererTests
 {
     [Fact]
-    public void Account_registration_form_keeps_required_legal_consents()
+    public void Account_registration_form_uses_checkbox_component_for_required_legal_consents()
     {
         var html = HtmlRenderer.AccountForm("/account/register", "Регистрация", name: true, registrationConsents: true);
 
-        Assert.Contains("class='form-consents'", html);
-        Assert.Contains("type='checkbox' name='acceptOffer' value='true' required", html);
-        Assert.Contains("type='checkbox' name='acceptPrivacy' value='true' required", html);
+        Assert.Contains("class='form-consents checkbox-list'", html);
+        Assert.Contains("<label class='checkbox-row'><input class='checkbox-control' type='checkbox' name='acceptOffer' value='true' required><span class='checkbox-text'>", html);
+        Assert.Contains("<label class='checkbox-row'><input class='checkbox-control' type='checkbox' name='acceptPrivacy' value='true' required><span class='checkbox-text'>", html);
+        Assert.Contains("</span></label>", html);
         Assert.Contains("/legal/offer?returnUrl=/account/register", html);
         Assert.Contains("/legal/rules?returnUrl=/account/register", html);
         Assert.Contains("/legal/client-consent?returnUrl=/account/register", html);
         Assert.Contains("/legal/privacy?returnUrl=/account/register", html);
+    }
+
+    [Fact]
+    public void Page_loads_common_checkbox_styles_before_payment_styles()
+    {
+        var html = HtmlRenderer.Page("Проверка", "<p>body</p>");
+
+        var checkboxIndex = html.IndexOf("href='/checkbox.css'", StringComparison.Ordinal);
+        var paymentIndex = html.IndexOf("href='/payment.css'", StringComparison.Ordinal);
+        Assert.True(checkboxIndex > 0, "checkbox.css must be linked globally.");
+        Assert.True(paymentIndex > checkboxIndex, "payment.css must not be the source of common checkbox styles.");
     }
 }
