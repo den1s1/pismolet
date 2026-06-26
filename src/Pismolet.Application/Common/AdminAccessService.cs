35dac0c158d14ce3b06e9dd8e497d5211399114a
den1s1 <den1s@mail.ru>
2026-06-26 20:06:25 +0300

    [Технарь] Расширить сервис админских прав

--- src/Pismolet.Application/Common/AdminAccessService.cs
+++ src/Pismolet.Application/Common/AdminAccessService.cs
@@ -3,4 +3,14 @@ namespace Pismolet.Web.Application.Common;
 public interface IAdminAccessService
 {
     bool IsAdminEmail(string? email);
+
+    bool IsConfigAdminEmail(string? email);
+
+    bool IsManagedAdminEmail(string? email);
+
+    IReadOnlyCollection<string> ListManagedAdminEmails();
+
+    void GrantAdmin(string email, string grantedByEmail);
+
+    bool TryRevokeAdmin(string email, string revokedByEmail, out string error);
 }
