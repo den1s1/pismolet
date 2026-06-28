3995f7f82b38ce7d3ec1d5683cfe5327f27de152
den1s1 <den1s@mail.ru>
2026-06-28 20:16:22 +0300

    [Технарь] Удалить пустой compatibility слой адресатов

--- src/Pismolet.Web/Endpoints/MailingRecipientFlowCompatibilityEndpoints.cs
+++
@@ -1,9 +0,0 @@
-namespace Pismolet.Web.Endpoints;
-
-public static class MailingRecipientFlowCompatibilityEndpoints
-{
-    public static IEndpointRouteBuilder MapMailingRecipientFlowCompatibilityEndpoints(this IEndpointRouteBuilder app)
-    {
-        return app;
-    }
-}
