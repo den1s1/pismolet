b90b251652c0da7a218fdc47b4aa151b732175aa
den1s1 <den1s@mail.ru>
2026-07-07 18:54:28 +0300

    [Технарь] Вернуть рабочий payform URL

--- src/Pismolet.Application/Common/ProdamusOptions.cs
+++ src/Pismolet.Application/Common/ProdamusOptions.cs
@@ -7,7 +7,7 @@ public sealed record ProdamusOptions(
     string ServiceName,
     bool IsTest)
 {
-    public const string DefaultPaymentPageUrl = "";
+    public const string DefaultPaymentPageUrl = "https://pismolet.payform.ru/";
     public const string DefaultServiceName = "Техническая подготовка и отправка email-рассылки по базе клиента";
 
     public bool HasPaymentPageUrl => !string.IsNullOrWhiteSpace(PaymentPageUrl);
