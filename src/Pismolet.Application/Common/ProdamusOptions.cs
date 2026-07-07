47d51c4d4facba66b064ce421731c4ab3b521bab
den1s1 <den1s@mail.ru>
2026-07-07 20:16:06 +0300

    [Технарь] Добавить параметры подписи Prodamus

--- src/Pismolet.Application/Common/ProdamusOptions.cs
+++ src/Pismolet.Application/Common/ProdamusOptions.cs
@@ -2,6 +2,8 @@ namespace Pismolet.Web.Application.Common;
 
 public sealed record ProdamusOptions(
     string PaymentPageUrl,
+    string PaymentPageSignatureKey,
+    string SysCode,
     string CallbackCheckValue,
     bool CallbackCheckRequired,
     string ServiceName,
@@ -11,5 +13,7 @@ public sealed record ProdamusOptions(
     public const string DefaultServiceName = "Техническая подготовка и отправка email-рассылки по базе клиента";
 
     public bool HasPaymentPageUrl => !string.IsNullOrWhiteSpace(PaymentPageUrl);
+    public bool HasPaymentPageSignatureKey => !string.IsNullOrWhiteSpace(PaymentPageSignatureKey);
     public bool HasCallbackCheckValue => !string.IsNullOrWhiteSpace(CallbackCheckValue);
+    public bool HasSysCode => !string.IsNullOrWhiteSpace(SysCode);
 }
