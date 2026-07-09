8ab6422ea23fabbfcef5ba980cbb6cfc566c1a5f
den1s1 <den1s@mail.ru>
2026-07-09 22:00:36 +0300

    [Технарь] Добавить снимок модели Mail.ru Postmaster

---
+++ src/Pismolet.Infrastructure/Migrations/Postmaster/MailruPostmasterDbContextModelSnapshot.cs
@@ -0,0 +1,46 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Pismolet.Web.Infrastructure.Postmaster;
+
+#nullable disable
+
+namespace Pismolet.Web.Infrastructure.Migrations.Postmaster;
+
+[DbContext(typeof(MailruPostmasterDbContext))]
+public partial class MailruPostmasterDbContextModelSnapshot : ModelSnapshot
+{
+    protected override void BuildModel(ModelBuilder modelBuilder)
+    {
+        modelBuilder.HasAnnotation("ProductVersion", "9.0.17");
+
+        modelBuilder.Entity<MailruPostmasterDomainDailyMetricEntity>(entity =>
+        {
+            entity.ToTable("mailru_postmaster_domain_daily_metrics");
+            entity.HasKey(x => x.Id);
+            entity.HasIndex(x => new { x.Domain, x.Date }).IsUnique();
+            entity.HasIndex(x => x.Date);
+            entity.Property(x => x.Domain).HasMaxLength(253).IsRequired();
+            entity.Property(x => x.Date).HasColumnType("date").IsRequired();
+        });
+
+        modelBuilder.Entity<MailruPostmasterTroubleSnapshotEntity>(entity =>
+        {
+            entity.ToTable("mailru_postmaster_trouble_snapshots");
+            entity.HasKey(x => x.Id);
+            entity.HasIndex(x => new { x.Domain, x.Code }).IsUnique();
+            entity.HasIndex(x => new { x.Domain, x.IsActive });
+            entity.Property(x => x.Domain).HasMaxLength(253).IsRequired();
+            entity.Property(x => x.Message).HasMaxLength(1000).IsRequired();
+        });
+
+        modelBuilder.Entity<MailruPostmasterSyncStateEntity>(entity =>
+        {
+            entity.ToTable("mailru_postmaster_sync_states");
+            entity.HasKey(x => x.Domain);
+            entity.Property(x => x.Domain).HasMaxLength(253).IsRequired();
+            entity.Property(x => x.LastErrorCode).HasMaxLength(120);
+            entity.Property(x => x.LastErrorSummary).HasMaxLength(1000);
+            entity.Property(x => x.LastDomainDate).HasColumnType("date");
+        });
+    }
+}
